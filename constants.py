fe19791bc9939f45deff7e89dc42d5dea9d571d2
fjauge <fjauge@gmail.com>
2014-03-12 10:41:36 +0100

    He separado en dos ficheros nuevos las constantes (facilidad para importar) y
    todo lo del gui (por claridad)

---
+++ constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#colors
+RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREY = (150,150,150)
+
+# files
+IMGDIR = "./data/sprites/"
+IMGTYPE ="png"
+
+# inicialitzations
+DIM_WIN = (1000,800)
+TYPES = {"red":RED,"blue":BLUE}
+CELL_NUM = 200
+FPS = 24
+
---
+++ game_gui.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+from constants import *
+from pgu import gui
+
+class ShowInfo(gui.Container):
+    ''' Quick Custom widget to show info and collaps it
+    we should refine a lottt parameters and other stuff'''
+    def __init__(self,**params):
+        gui.Container.__init__(self,**params)
+        self.data,self.button = None,None
+
+    def text(self,text):
+        if self.widgets!=[]:
+            self.data.value=text
+
+    def addinfo(self,title,text):
+        self.widgets = []
+        name=gui.Button(title)
+        data=gui.TextArea(text,size=10,width=100,height=150,disabled=True)
+        data.value=text
+        self.data=data
+        self.button=name.value
+        table=gui.Table()
+        table.tr();table.td(name);table.tr();table.td(data)
+        name.connect(gui.CLICK,self.remove,table)
+        self.add(table,0,0)
+
+def update_gui(self):
+    #write number of cells by type
+    for tipo in self.mundo.tipos:
+        self.form['num_'+tipo].value= \
+                '{:>14}'.format(len(self.mundo.popul_indexs[tipo]))
+    #write data on selected cell
+    info=self.form['info']
+    if info.widgets!=[] and info.button.value in self.mundo.population:
+        text='';cell=self.mundo.population[info.button.value]
+        for attr in ['tipo','armor','hp','dps','sons','energy']:
+            text=text+attr+':'+str(getattr(cell,attr))+'\n'
+        info.text(text)
+
+def verify_mouse(self,button,pos):
+    foundcell=False
+    if pos[0]<self.mundo.map_dim[0]:
+        if button==1:
+            for cell in self.mundo.population.values():
+                if cell.pos.get_distance(pos)<50:
+                    foundcell=cell
+                    break
+    if foundcell:
+        info=self.form['info']; text=''
+        for attr in ['tipo','armor','hp','dps','sons','energy']:
+            text=text+attr+':'+str(getattr(foundcell,attr))+'\n'
+        info.addinfo(foundcell.name,text)
+
+def create_widgets(self):
+    '''creates an returns GUI structure a la DOM, that will be asigned to
+    self.container from wich startgui can start its painting
+    as startgui is called from the parent class sometimes we wont have al
+    the atributes as we need, so is useful to call it again at the end
+    of the init of the new class'''
+
+    g=self.gui # to abreviate the use
+    #controlling we have al the atributes we need
+    for attr in ['fps','tipos']:
+        if not hasattr(self,attr):
+            return g.Container(align=-1,valign=-1)
+    def gui_speed(slider):
+        self.fps=slider.value
+        self.form['label_speed'].value='Speed:{:>4}'.format(self.fps)
+
+    def gui_quantity(slider):
+        self.form['label_qtty'].value='Qtty:{:>5}'.format(slider.value)
+    def gui_run(_button):
+        self.game_running= not self.game_running
+        _button.value='Pause' if self.game_running else 'Start'
+
+    def gui_restart(_button):
+        self.mundo.population,self.mundo.deads = {},{}
+        self.mundo.populate(self.form['quantity'].value)
+
+    # try to be able to reescale the widgets
+    table=g.Table(name='tabla')
+    fg = BLACK
+
+    table.tr()
+    e=g.Button('Start', name='run',size=10)
+    e.connect(g.CLICK, gui_run, e)
+    table.td(e)
+
+    table.tr()
+    table.td(g.Label('Speed:{:>4}'.format(self.fps),color=fg, \
+                       name='label_speed',size=10), align=-1)
+    table.tr()
+    e = g.HSlider(self.fps,1,50,size=10,width=100,height=16,name='speed')
+    e.connect(g.CHANGE, gui_speed, e)
+    table.td(e, align=-1)
+
+    table.tr()
+    table.td(g.Container(height=5,size=10))
+
+    table.tr()
+    e=g.Button('Restart', name='restart',size=10)
+    e.connect(g.CLICK, gui_restart, e)
+    table.td(e)
+
+    table.tr()
+    cant= len(self.mundo.population)
+    table.td(g.Label('Qtty:{:>5}'.format(cant),color=fg,\
+              name='label_qtty',size=10),align=-1)
+    table.tr()
+    e =g.HSlider(cant,1,999,size=10,width=100,height=16,name='quantity')
+    e.connect(g.CHANGE, gui_quantity, e)
+    table.td(e,align=-1)
+    for tipo in self.tipos:
+        table.tr()
+        color=self.tipos[tipo][1]
+        cant='{:>14}'.format(len(self.mundo.popul_indexs[tipo]))
+        table.td(g.Label(value=cant,color=color,name='num_'+tipo,\
+                            size=10),align=-1)
+    table.tr()
+    info=ShowInfo(width=100,name='info')
+    table.td(info)
+
+    cont=g.Container(width=100,height=self.size[1],\
+                       background=GREY,align=-1)
+    cont.add(table,0,0)
+    container=g.Container()#width=self.size[0],height=self.size[1],align=-1,valign=-1)
+    container.add(cont,self.size[0]-100,0)
+    return container
+
